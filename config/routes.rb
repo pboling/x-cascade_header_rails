780fa0ad0aaf8c25b3492743c5ced452d012f97e
Peter Boling <peter.boling@gmail.com>
2014-06-20 13:41:12 -0700

    Setup Rack routes handlers and integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'none' => proc { |env| [401, {}, ["Not Authorized"]] }
-  get 'empty' => proc { |env| [401, {'X-Cascade' => ''}, ["Not Authorized"]] }
-  get 'pass' => proc { |env| [401, {'X-Cascade' => 'pass'}, ["Not Authorized"]] }
+
+  # The buck stops here
+  get 'none'  => proc { |env| [401, {},                       ["Not Authorized"]] }
+  get 'empty' => proc { |env| [401, {'X-Cascade' => ''},      ["Not Authorized"]] }
+
+  # This route will not respond to the request, but cascade back into this route file
+  # and hit the next route that matches the request URI
+  get 'cascade/401' => proc { |env| [401, {'X-Cascade' => 'pass'},  ["Not Authorized"]] }
+  get 'cascade/:id' => proc { |env| [200, {},  ["Welcome"]] }
+
 end
