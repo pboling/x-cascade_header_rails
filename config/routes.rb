6e5ccb90732ab2c914be8141bbf6cc90dabddda0
Peter Boling <peter.boling@gmail.com>
2014-06-20 15:13:20 -0700

    better routes file
    Conflicts:
    	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
-XCascadeHeaderRails::Application.routes.draw do
+Rails.application.routes.draw do
   devise_for :users
-
   root to: 'home#index'
 
   # The buck stops here
   match 'none'  => proc { |env| [401, {},                       ["Not Authorized"]] }
+  # So this one doesn't get hit, because header X-Cascade != 'pass'
+  match 'none'  => proc { |env| [200, {},                       ["Welcome"]] }
+
+  # The buck stops here
   match 'empty' => proc { |env| [401, {'X-Cascade' => ''},      ["Not Authorized"]] }
+  # So this one doesn't get hit, because header X-Cascade != 'pass'
+  match 'empty' => proc { |env| [200, {},                       ["Welcome"]] }
 
   # This route will not respond to the request, but cascade back into this route file
   # and hit the next route that matches the request URI
   match 'cascade/401' => proc { |env| [401, {'X-Cascade' => 'pass'},  ["Not Authorized"]] }
-  match 'cascade/:id' => proc { |env| [200, {},  ["Welcome"]] }
+  match 'cascade/:id' => proc { |env| [200, {},                 ["Welcome"]] }
 end
