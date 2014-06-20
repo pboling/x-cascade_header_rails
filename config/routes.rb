e5b3d2ecdcac0aaaa5cb69afd64463ec7f111382
Peter Boling <peter.boling@gmail.com>
2014-06-20 13:44:20 -0700

    integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 XCascadeHeaderRails::Application.routes.draw do
-  match 'none' => proc { |env| [401, {}, ["Not Authorized"]] }
-  match 'empty' => proc { |env| [401, {'X-Cascade' => ''}, ["Not Authorized"]] }
-  match 'pass' => proc { |env| [401, {'X-Cascade' => 'pass'}, ["Not Authorized"]] }
+  # The buck stops here
+  match 'none'  => proc { |env| [401, {},                       ["Not Authorized"]] }
+  match 'empty' => proc { |env| [401, {'X-Cascade' => ''},      ["Not Authorized"]] }
+
+  # This route will not respond to the request, but cascade back into this route file
+  # and hit the next route that matches the request URI
+  match 'cascade/401' => proc { |env| [401, {'X-Cascade' => 'pass'},  ["Not Authorized"]] }
+  match 'cascade/:id' => proc { |env| [200, {},  ["Welcome"]] }
 end
---
+++ test/integration/x_cascade_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class XCascadeTest < ActionDispatch::IntegrationTest
+  test '/none is 401 error' do
+    get '/none'
+    assert_equal path, '/none'
+    assert_response 401, @response.body # show the response body if the assertion fails for debugging!
+  end
+
+  test '/empty is 401 error' do
+    get '/empty'
+    assert_equal path, '/empty'
+    assert_response 401, @response.body # show the response body if the assertion fails for debugging!
+  end
+
+  test '/cascade/401 is 200 error' do
+    get '/cascade/401'
+    assert_equal path, '/cascade/401'
+    assert_response 200, @response.body # show the response body if the assertion fails for debugging!
+  end
+
+end
