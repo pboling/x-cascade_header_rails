eab3d2272734b3e6348a1495b606d66406e920b7
Peter Boling <peter.boling@gmail.com>
2014-06-21 00:03:32 -0700

    Comment failure

--- test/integration/x_cascade_grape_test.rb
+++ test/integration/x_cascade_grape_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class XCascadeGrapeTest < ActionDispatch::IntegrationTest
 
+  # Rack Session is not compatible with ActionDispatch::Request::Session, so this test fails.
+  # Switching to Rspec & Capybara for now.
   test '/api/auth/login invalid credentials is 401' do
     get '/api/auth/login'
     assert_equal path, '/api/auth/login'
